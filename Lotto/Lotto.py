d137f92404984110d91fc5aa1718fd37f79956f6
MarekPas <58218081+MarekPas@users.noreply.github.com>
2020-08-11 10:28:16 +0300

    Update Lotto.py
    cosmetic changes

--- Lotto/Lotto.py
+++ Lotto/Lotto.py
@@ -5,24 +5,24 @@ import random
 lotto = list(np.arange(1,37))
 
 def get_result(numbers):
-    x = random.sample(lotto, 6)
+    draw = random.sample(lotto, 6)
     # x = secrets.SystemRandom().sample(lotto, 6) # secrets lib is another random method
-    x.sort()
-    print("Drawn numbers: ", x)
+    draw.sort()
+    print("Drawn numbers: ", draw)
     result = 0
-    for i in x:
+    for i in draw:
         if i in numbers:
             result += 1
     return print(f"You bet correctly {result} number(s)!") 
 
 def run():
-    i=0
+    i = 0
     numbers = []
     while i != 6:
         try:
             n1 = int(input(f'Enter {i+1} number: '))
         except ValueError:
-            print("You may enter number only from 1 to 36!")
+            print("You may enter number from 1 to 36 only!")
             continue
         if n1 > 36 or n1 < 1:
             print("Number must be between 1 and 36!")
@@ -36,11 +36,6 @@ def run():
     numbers.sort()
     print("Your numbers: ", numbers)
     get_result(numbers)
-    ## How many times do you want to draw?
-##    c = 10
-##    while c != 0:
-##        get_result(numbers)
-##        c -= 1
 
 if __name__ == "__main__":
     run()
