1a86c27c3504adff61e810077f4db8b285d0e4f7
MarekPas <58218081+MarekPas@users.noreply.github.com>
2020-08-17 19:48:22 +0300

    Update Lotto.py

--- Lotto/Lotto.py
+++ Lotto/Lotto.py
@@ -1,26 +1,22 @@
 import numpy as np
-import secrets
+##import secrets
 import random
 
 lotto = list(np.arange(1,37))
 
 def get_result(numbers):
     draw = random.sample(lotto, 6)
-    # x = secrets.SystemRandom().sample(lotto, 6) # secrets lib is another random method
+##    draw = secrets.SystemRandom().sample(lotto, 6) # secrets lib is another random method
     draw.sort()
     print("Drawn numbers: ", draw)
-    result = 0
-    for i in draw:
-        if i in numbers:
-            result += 1
+    result = len([True for i in draw if i in numbers])
     return print(f"You bet correctly {result} number(s)!") 
 
 def run():
-    i = 0
     numbers = []
-    while i != 6:
+    while len(numbers) != 6:
         try:
-            n1 = int(input(f'Enter {i+1} number: '))
+            n1 = int(input(f'Enter {len(numbers)+1} number: '))
         except ValueError:
             print("You may enter number from 1 to 36 only!")
             continue
@@ -31,7 +27,6 @@ def run():
             if n1 in numbers:
                 print("You have already entered this number. Try another one.")
             else:
-                i += 1
                 numbers.append(n1)
     numbers.sort()
     print("Your numbers: ", numbers)
