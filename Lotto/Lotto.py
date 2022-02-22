0d8fe0bafc3e5c7a40bfb610d6c7662705195803
MarekPas <58218081+MarekPas@users.noreply.github.com>
2022-02-22 21:07:52 +0300

    Update Lotto.py
    rearranged some variables and simplified the code

--- Lotto/Lotto.py
+++ Lotto/Lotto.py
@@ -1,38 +1,49 @@
-import numpy as np
 ##import secrets
 import random
+import sys
 
-lotto = list(np.arange(1,37))
+LOTTO = list(range(1,37))
 
 def get_result(numbers):
-    draw = random.sample(lotto, 6)
-##    draw = secrets.SystemRandom().sample(lotto, 6) # secrets lib is another random method
+    """Draws and checks if 6 given numbers are same as drawn numbers from LOTTO list
+
+    Args:
+        numbers (list): list of unique numbers
+
+    Returns:
+        str: how many numbers we bet correctly
+    """
+    draw = random.sample(LOTTO, 6)
+##    draw = secrets.SystemRandom().sample(LOTTO, 6) # secrets lib is another random method
     draw.sort()
     print("Drawn numbers: ", draw)
     result = len([True for i in draw if i in numbers])
-    return print(f"You bet correctly {result} number(s)!") 
+    return f"You bet correctly {result} number(s)!"
 
 def run():
+    """Asks an user to enter 6 numbers"""
     numbers = []
     while len(numbers) != 6:
         try:
-            n1 = int(input(f'Enter {len(numbers)+1} number: '))
+            number = int(input(f'Enter {len(numbers)+1} number: '))
         except ValueError:
-            print("You may enter number from 1 to 36 only!")
+            print(f"You may enter numbers from {LOTTO[0]} to {LOTTO[-1]} only!")
             continue
-        if n1 > 36 or n1 < 1:
-            print("Number must be between 1 and 36!")
-            continue
+        if number > LOTTO[-1] or number < LOTTO[0]:
+            print(f"Number must be between {LOTTO[0]} and {LOTTO[-1]}!")
         else:
-            if n1 in numbers:
+            if number in numbers:
                 print("You have already entered this number. Try another one.")
             else:
-                numbers.append(n1)
+                numbers.append(number)
     numbers.sort()
     print("Your numbers: ", numbers)
-    get_result(numbers)
+    print(get_result(numbers))
+    again = input("Do you want to try again? (y/n) ")
+    if again.lower() == "y" or again.lower() == "yes":
+        run()
+    else:
+        sys.exit(0)
 
 if __name__ == "__main__":
     run()
-
-
